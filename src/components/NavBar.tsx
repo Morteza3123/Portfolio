e69e60e3318426f0458db1138e713e9653b41d75
Morteza3123 <mkhedry3123@gmail.com>
2022-07-03 01:55:17 +0430

    first commit

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,50 @@
+import React, { useState } from 'react'
+import {FaBars, FaTimes} from 'react-icons/fa'
+
+const NavBar = () => {
+
+    const [nav, setNav] = useState(false)
+
+    const links = [
+        {
+            id: 1,
+            link: 'home'
+        },
+        {
+            id: 2,
+            link: 'about'
+        },
+        {
+            id: 3,
+            link: 'portfolio'
+        },
+        {
+            id: 4,
+            link: 'experience'
+        },
+        {
+            id: 5,
+            link: 'contact'
+        },
+    ]
+  return (
+    <div className='flex justify-between items-center w-full h-20 text-white fixed bg-black px-4'>
+        <div>
+            <h1 className='text-4xl font-signature ml-2'>Morteza</h1>
+        </div>
+
+        <ul className='hidden md:flex'>
+
+            {links.map(({id, link}) => (
+                <li key={id} className='px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 duration-200'>{link}</li>
+            ))}
+        </ul>
+
+        <div onClick={() => setNav(!nav)} className='cursor-pointer pr-4 z-10 text-gray-100'>
+           {nav ? <FaTimes size={30} /> : <FaBars size={30}/>}
+        </div>
+    </div>
+  )
+}
+
+export default NavBar
