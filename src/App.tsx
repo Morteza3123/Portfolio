2f8b2dab0aab75d501dc25d523c383d0d68a8ba5
Morteza3123 <mkhedry3123@gmail.com>
2022-07-03 22:06:55 +0430

    add introduce section

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import Home from "./components/Home";
 import NavBar from "./components/NavBar";
+import SocialLinks from "./components/SocialLinks";
 
 function App() {
   return (
     <div>
       <NavBar/>
+      <Home />
+      <SocialLinks />
     </div>
   );
 }
