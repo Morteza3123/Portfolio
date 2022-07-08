0d736911d66ce0aa9aacf3d277ae3819ccb780e8
Morteza3123 <mkhedry3123@gmail.com>
2022-07-09 02:24:28 +0430

    edit nav zindex

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -28,7 +28,7 @@ const NavBar = () => {
     },
   ];
   return (
-    <div className="flex justify-between items-center w-full h-16 px-4 text-white bg-black fixed">
+    <div className="z-40 backdrop-brightness-0 flex justify-between items-center w-full h-16 px-4 text-white bg-black/0 fixed">
       <div>
         <h1 className="text-5xl font-signature ml-2">Morteza</h1>
       </div>
@@ -37,7 +37,7 @@ const NavBar = () => {
         {links.map(({ id, link }) => (
           <li
             key={id}
-            className="px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 duration-200"
+            className="px-4 cursor-pointer capitalize font-medium text-gray-400 hover:scale-105 duration-200"
           >
            <Link to={link} smooth duration={500}>{link}</Link>
           </li>
