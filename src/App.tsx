f3e71d50b88495ab34e69878acfb324a5249384e
Morteza3123 <mkhedry3123@gmail.com>
2022-07-16 15:14:23 +0430

    add chat box successfully

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Exprience from "./components/Exprience";
@@ -8,7 +9,21 @@ import SocialLinks from "./components/SocialLinks";
 import "./styles.css";
 
 
+
 function App() {
+
+  useEffect(() => {
+    window.$crisp = [];
+    window.CRISP_WEBSITE_ID = "ed217160-f41e-49f5-a6c3-9f14e8aac257";
+    (function () {
+      var d = document;
+      var s = d.createElement("script");
+      s.src = "https://client.crisp.chat/l.js";
+      s.async = true;
+      d.getElementsByTagName("head")[0].appendChild(s);
+    })();
+  });
+  
   return (
     <div>
       <NavBar />
--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -3,5 +3,7 @@ export{}
 declare global {
     interface Window {
         CometChatWidget: any;
+        $crisp: any;
+        CRISP_WEBSITE_ID:any;
     }
   }
